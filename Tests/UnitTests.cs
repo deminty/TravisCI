5311efd219eb8dc84099d43c54c8837e1243db41
deminty <31461024+deminty@users.noreply.github.com>
2019-03-30 10:14:12 -0500

    fixed failing unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -13,7 +13,6 @@ namespace TravisCILab
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
             Assert.AreEqual(12, Program.Add("5", "7"));
-            Assert.Fail();
         }
 
         [Test]
